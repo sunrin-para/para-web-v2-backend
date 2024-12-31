aa578e1b9e7ec8d65eb044ade67faee51248b473
Juan Lee <ninejuan@protonmail.com>
2024-12-31 21:30:20 +0900

    docs: cors, swagger setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'http://127.0.0.1:3000',
+      'http://127.0.0.1:5173',
+    ],
+    credentials: true,
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization', 'Set-Cookie'],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle(process.env.NAME)
+    .setDescription(process.env.DESCRIPTION)
+    .setVersion(process.env.VERSION)
+    .addTag(process.env.TAG)
+    .addCookieAuth('sessionId')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document, {
+    jsonDocumentUrl: 'api-docs/json',
+    explorer: true,
+    yamlDocumentUrl: 'api-docs/yaml',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+
+bootstrap()
+  .then(() => console.log('PARA Web API Started successfully'))
+  .catch((error) => console.error('Failed to start PARA Web API:', error));
