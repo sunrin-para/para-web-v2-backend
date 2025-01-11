478ab0879d5a7d9d9b2718b4bbd182cebcf36db5
jureuk7 <jureuk7@protonmail.com>
2025-01-11 18:38:12 +0900

    feat: basic modules init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MemberService } from './member/member.service';
+import { MemberModule } from './member/member.module';
+import { AuthModule } from './auth/auth.module';
+import { QuestionModule } from './question/question.module';
+import { PortfolioModule } from './portfolio/portfolio.module';
 
 @Module({
-  imports: [],
+  imports: [MemberModule, AuthModule, QuestionModule, PortfolioModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MemberService],
 })
 export class AppModule {}
