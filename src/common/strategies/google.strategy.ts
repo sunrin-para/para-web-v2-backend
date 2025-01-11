95dc5b58ebec9d6a3b69dc9e948638bdc33774bf
Juan Lee <ninejuan@protonmail.com>
2025-01-11 23:23:35 +0900

    add: google strategy

---
+++ src/common/strategies/google.strategy.ts
@@ -0,0 +1,38 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { AuthService } from 'src/auth/auth.service';
+
+const env = process.env;
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      clientID: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+      // callbackURL: `${env.HTTP_PROTOCOL}://${env.DOMAIN}${env.GOOGLE_CALLBACK_PARAM}`,
+      callbackURL: `${env.DOMAIN}${env.GOOGLE_REDIRECT_URL}`,
+      scope: ['profile', 'email'],
+    });
+  }
+
+  private async isValidEmail(email) {
+    const regex = /^[a-zA-Z0-9._%+-]+@(sunrint\.hs\.kr|sunrin-para\.dev)$/;
+    return regex.test(email);
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const user = profile;
+    user.accessToken = accessToken;
+    user.refreshToken = refreshToken;
+    console.log(user._json.email);
+    if (this.isValidEmail(user._json.email)) return done(null, profile);
+    else throw new ForbiddenException();
+  }
+}
