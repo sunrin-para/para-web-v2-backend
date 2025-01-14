2b24fb6ff44de11d76a3c9188db674504141bcec
Juan Lee <ninejuan@protonmail.com>
2025-01-14 10:45:00 +0900

    feat: Jwtpayload dto

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -26,7 +26,13 @@ export class AuthService {
     return user;
   }
 
-  async generate
+  // 만들어야 할 함수들
+  // generateToken(ac, ref), refreshToken, invalidate refreshtoken, 
+  async generateToken(type: string = "access") {
+    // const payload: JwtPayload = {}
+    // switch()
+  }
+
   // 여기서 bcrypt 이용해서 비밀번호 맞는지 검사해야 함.
   // case 2 : google or local strategy로 먼저 가입된 user의 경우, 기존 schema를 업데이트 해주는 방향으로 가야 함.
   // case 2의 경우 local auth는 admin page에서만 등록 가능하기 때문에 무조건적인 업데이트를 해줘도 됨.
