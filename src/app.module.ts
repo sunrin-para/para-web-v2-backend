f74956a756beaee242f02118192e78d7bc79af41
Juan Lee <ninejuan@protonmail.com>
2025-02-08 00:35:37 +0900

    add: question default res

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { PrismaService } from './prisma/prisma.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserService } from './user/user.service';
 import { UserModule } from './user/user.module';
+import { QuestionsModule } from './questions/questions.module';
 
 @Module({
-  imports: [AuthModule, PrismaModule, UserModule],
+  imports: [AuthModule, PrismaModule, UserModule, QuestionsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService, UserService],
 })
