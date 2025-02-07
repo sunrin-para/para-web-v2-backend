92e693c7216f26eb6a477f6bf0087205bb434e0b
Juan Lee <ninejuan@protonmail.com>
2025-02-08 00:43:53 +0900

    add: questions controller base

--- src/questions/questions.controller.ts
+++ src/questions/questions.controller.ts
@@ -1,9 +1,38 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Patch,
+  Post,
+  SetMetadata,
+  UseGuards,
+} from '@nestjs/common';
 import { QuestionsService } from './questions.service';
+import { AdminGuard } from 'src/common/guards/admin.guard';
+import { CreateFAQDto } from './dto/register.dto';
+import { EditFAQDto } from './dto/edit.dto';
+import { DeleteFAQDto } from './dto/delete.dto';
 
 @Controller('questions')
 export class QuestionsController {
   constructor(private readonly questionsService: QuestionsService) {}
 
-  
+  @Post()
+  @UseGuards(AdminGuard)
+  @SetMetadata('permission', 'NOT SET')
+  async createFAQ(@Body() createFAQDto: CreateFAQDto) {}
+
+  @Get()
+  async getAllFAQ() {}
+
+  @Patch()
+  @UseGuards(AdminGuard)
+  @SetMetadata('permission', 'NOT SET')
+  async editFAQ(@Body() editFAQDto: EditFAQDto) {}
+
+  @Delete()
+  @UseGuards(AdminGuard)
+  @SetMetadata('permission', 'NOT SET')
+  async DeleteFAQDto(@Body() deleteFAQDto: DeleteFAQDto) {}
 }
