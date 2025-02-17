0136ea0fa33ed48de19595307b715f3f6976187c
Juan Lee <ninejuan@protonmail.com>
2025-02-16 20:03:08 -1000

    add: gallary model

--- src/gallary/gallary.controller.ts
+++ src/gallary/gallary.controller.ts
@@ -1,8 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  SetMetadata,
+  UseGuards,
+} from '@nestjs/common';
 import { GallaryService } from './gallary.service';
+import { AdminGuard } from 'src/common/guards/admin.guard';
 
 @Controller('gallary')
 export class GallaryController {
   constructor(private readonly gallaryService: GallaryService) {}
-
 }
