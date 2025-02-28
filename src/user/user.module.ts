ba5c19cb0fbb947d2303398bf6bed27731459963
Juan Lee <ninejuan@protonmail.com>
2025-02-28 14:57:48 +0900

    refactor: 기본 관리자 계정 생성을 scripts로 분리했습니다.

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/common/prisma/prisma.service';
+import { UserController } from './user.controller';
 
 @Module({
   providers: [PrismaService],
+  controllers: [UserController],
 })
 export class UserModule {}
