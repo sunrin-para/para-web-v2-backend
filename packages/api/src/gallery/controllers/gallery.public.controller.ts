4eb8d00b44cb752163d4c09ef8775a4799f1ab7b
Juan Lee <ninejuan@protonmail.com>
2025-03-09 23:53:23 +0900

    📝 Docs: add swagger docs - Gallery public controller

--- packages/api/src/gallery/controllers/gallery.public.controller.ts
+++ packages/api/src/gallery/controllers/gallery.public.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param } from '@nestjs/common';
 import { GalleryService } from '@/gallery/gallery.service';
 import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
@@ -6,14 +6,53 @@ import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 export class GalleryPublicController {
   constructor(private readonly galleryService: GalleryService) {}
   @Get('/detail/:albumId')
+  @ApiResponse({
+    status: 200,
+    description: '앨범 상세 조회 성공',
+    schema: {
+      type: 'object',
+      properties: {
+        title: { type: 'string' },
+        date: { type: 'string' },
+        photos: {
+          type: 'array',
+          items: {
+            type: 'string',
+          },
+        },
+      },
+    },
+  })
   async getAlbumDetail(@Param('albumId') albumId: number) {
     return await this.galleryService.getAlbumDetail(albumId);
   }
 
   @ApiOperation({ summary: '모든 앨범 조회' })
+  @HttpCode(200)
   @ApiResponse({
     status: 200,
     description: '모든 앨범 조회 성공',
+    schema: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          year: { type: 'number' },
+          albums: {
+            type: 'array',
+            items: {
+              type: 'object',
+              properties: {
+                id: { type: 'number' },
+                title: { type: 'string' },
+                date: { type: 'string' },
+                thumbnailUrl: { type: 'string' },
+              },
+            },
+          },
+        },
+      },
+    },
   })
   @Get()
   async getAllAlbums() {
@@ -21,9 +60,27 @@ export class GalleryPublicController {
   }
 
   @ApiOperation({ summary: '연도별 앨범 조회' })
+  @HttpCode(200)
   @ApiResponse({
     status: 200,
     description: '연도별 앨범 조회 성공',
+    schema: {
+      type: 'object',
+      properties: {
+        year: { type: 'number' },
+        albums: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              title: { type: 'string' },
+              date: { type: 'string' },
+              thumbnailUrl: { type: 'string' },
+            },
+          },
+        },
+      },
+    },
   })
   @Get('/year/:year')
   async getAlbumsByYear(@Param('year') year: number) {
