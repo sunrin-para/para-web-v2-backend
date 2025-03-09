4d58161dc190abd60ee1d527c52ee9c03090c233
Juan Lee <ninejuan@protonmail.com>
2025-03-09 21:40:55 +0900

    ✏️ Fix typo: Gallary -> Gallery

--- packages/api/src/gallery/dto/update-album.dto.ts
+++ packages/api/src/gallery/dto/update-album.dto.ts
@@ -1,6 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsArray, IsDate, IsOptional, IsString } from 'class-validator';
 
 export class UpdateAlbumDto {
+  @IsString()
+  @IsOptional()
   @ApiProperty({
     description: '앨범 제목',
     example: '2024 선린 축제',
@@ -8,6 +12,8 @@ export class UpdateAlbumDto {
   })
   title?: string;
 
+  @IsString()
+  @IsOptional()
   @ApiProperty({
     description: '앨범 설명',
     example: '2024년도 선린인터넷고등학교 축제 사진 모음',
@@ -15,6 +21,9 @@ export class UpdateAlbumDto {
   })
   description?: string;
 
+  @IsDate()
+  @IsOptional()
+  @Type(() => Date)
   @ApiProperty({
     description: '앨범 날짜',
     example: '2024-05-20T00:00:00.000Z',
@@ -22,6 +31,9 @@ export class UpdateAlbumDto {
   })
   date?: Date;
 
+  @IsArray()
+  @IsOptional()
+  @Type(() => Array)
   @ApiProperty({
     description: '삭제할 사진 인덱스 배열',
     example: [0, 1, 2],
