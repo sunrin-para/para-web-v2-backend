619424d82515e97b5e7875b37e19af9f4139adc2
Juan Lee <ninejuan@protonmail.com>
2025-03-10 03:27:07 +0900

    ♻️ Refactor: FAQ

--- packages/api/src/questions/questions.service.ts
+++ packages/api/src/questions/questions.service.ts
@@ -1,32 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateFAQDto } from './dto/register.dto';
 import { QuestionsRepository } from './repository/questions.repo';
-import { FAQDto } from './dto/get.dto';
 
 @Injectable()
 export class QuestionsService {
   constructor(private readonly questionsRepository: QuestionsRepository) {}
   async createFaq(createFAQDto: CreateFAQDto) {
-    // 여기서 해킹구문 같은거 확인하고, 있으면 BadRequestException 발생시키기
-    const newFaq = await this.questionsRepository.addFaq(createFAQDto);
-    return newFaq;
+    return await this.questionsRepository.addFaq(createFAQDto);
   }
 
-  async getAllFaq(): Promise<FAQDto[]> {
-    const faqs: FAQDto[] = await this.questionsRepository.getAllFaq();
-    return faqs;
+  async getAllFaq() {
+    return await this.questionsRepository.getAllFaq();
   }
 
   async updateFaq(faqId: number, editFAQDto: CreateFAQDto) {
-    const updatedFaq = await this.questionsRepository.updateFaq(
-      faqId,
-      editFAQDto,
-    );
-    return updatedFaq;
+    return await this.questionsRepository.updateFaq(faqId, editFAQDto);
   }
 
   async deleteFaq(faqId: number) {
-    const result = await this.questionsRepository.deleteFaq(faqId);
-    return result;
+    return await this.questionsRepository.deleteFaq(faqId);
   }
 }
