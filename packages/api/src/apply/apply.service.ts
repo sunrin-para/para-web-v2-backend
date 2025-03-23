7bc8b16455da4636b87c018bb9ed57738a075c83
Juan Lee <me@juany.kr>
2025-03-23 19:48:09 +0900

    🗑️ Removed apply res

--- packages/api/src/apply/apply.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { ApplyRepository } from './repository/apply.repo';
-
-@Injectable()
-export class ApplyService {
-  constructor(private readonly applyRepository: ApplyRepository) {}
-}
--- packages/api/src/apply/repository/apply.repo.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '@/common/prisma/prisma.service';
-
-@Injectable()
-export class ApplyRepository {
-  constructor(private readonly prismaService: PrismaService) {}
-}
