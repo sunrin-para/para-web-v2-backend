a5278ec311fba8591e4c8f15e820be714165d493
jureuk7 <to@jureuk.dev>
2026-02-13 01:09:41 +0900

    fix: Update TypeScript resolutions and checksums in yarn.lock; enhance portfolio controller to include Query parameter for improved data retrieval

--- packages/api/src/portfolio/controllers/portfolio.public.controller.ts
+++ packages/api/src/portfolio/controllers/portfolio.public.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common'
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common'
 import { PortfolioService } from '@/portfolio/portfolio.service'
 import { ApiOperation, ApiResponse, ApiQuery } from '@nestjs/swagger'
 import { PortfolioDto } from '../dto/portfolio.dto'
